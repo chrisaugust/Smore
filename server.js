2e9f43ba4952fa3b078e7ed4164da553dd54ce3f
Chris Danielson <christopher.a.danielson@protonmail.com>
2024-10-24 21:03:08 -0400

    Add default port value for Heroku config

--- server.js
+++ server.js
@@ -368,6 +368,9 @@ app.get('*', (req, res) => {
 
 
 const port = process.env.PORT;
+if (port == null || port == "") {
+  port = 8000;
+}
 app.listen(port, () => {
   console.log(`Server up and listening on port ${port}`);
 });
