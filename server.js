7516d313f8344be66346b00d1206f7d9d82fa212
Chris Danielson <christopher.a.danielson@protonmail.com>
2024-10-24 22:19:21 -0400

    Modify db connection details to satisfy Heroku

--- server.js
+++ server.js
@@ -13,11 +13,15 @@ app.use(morgan('dev'));
 app.use(cors());
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT
+//  user: process.env.DB_USER,
+//  host: process.env.DB_HOST,
+//  database: process.env.DB_NAME,
+//  password: process.env.DB_PASSWORD,
+//  port: process.env.DB_PORT
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 const JWT_SECRET = process.env.JWT_SECRET;
